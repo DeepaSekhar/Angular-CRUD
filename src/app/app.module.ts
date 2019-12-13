3d87eb380f2f4fcbb521c00c4bf9737cecda9b53
deepaakhil84 <deepaakhiltvm@gmail.com>
2019-12-13 14:35:28 +0000

    adding link to rooting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, Component } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ListEmployeesComponent } from './employees/list-employees.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ListEmployeesComponent } from "./employees/list-employees.component";
+import { CreateEmployeesComponent } from "./employees/create-employees.component";
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+  {
+    path: "list",
+    component: ListEmployeesComponent
+  },
+  {
+    path: "create",
+    component: CreateEmployeesComponent
+  },
+  {
+    path: "",
+    redirectTo: "/list",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListEmployeesComponent
+    ListEmployeesComponent,
+    CreateEmployeesComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
