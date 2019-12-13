eee64a421e8057780c026ddc1f4a7a0fc8265463
deepaakhil84 <deepaakhiltvm@gmail.com>
2019-12-13 17:16:46 +0000

    linking page with navbar

--- src/app/employees/create-employees.component.ts
+++ src/app/employees/create-employees.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-create-employees',
-  templateUrl: './create-employees.component.html',
-  styleUrls: ['./create-employees.component.css']
+  selector: "app-create-employees",
+  templateUrl: "./create-employees.component.html",
+  styleUrls: ["./create-employees.component.css"]
 })
 export class CreateEmployeesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
