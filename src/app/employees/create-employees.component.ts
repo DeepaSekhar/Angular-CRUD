0e30bc94f1611d288973d1ba61366e12f24b58ac
deepaakhil84 <deepaakhiltvm@gmail.com>
2019-12-19 11:09:58 +0000

    changed hardcorded list to a class of list

--- src/app/employees/create-employees.component.ts
+++ src/app/employees/create-employees.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Department } from "../model/department.model";
 @Component({
   selector: "app-create-employees",
   templateUrl: "./create-employees.component.html",
   styleUrls: ["./create-employees.component.css"]
 })
 export class CreateEmployeesComponent implements OnInit {
+  departments: Department[] = [
+    { id: 1, name: "Help Desk" },
+    { id: 2, name: "IT" },
+    { id: 3, name: "HR" }
+  ];
   constructor() {}
 
   ngOnInit() {}
